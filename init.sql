5786de968c1eaac2f63b559a46710a99e891c8a5
abdulrhman <abdulrhmanmokhtar399@gmail.com>
2026-01-29 23:30:08 +0200

    the final solution for CI

--- init.sql
+++ init.sql
@@ -10,3 +10,8 @@ CREATE TABLE IF NOT EXISTS products (
     name VARCHAR(255) NOT NULL,
     price DECIMAL(10,2) NOT NULL
 );
+
+INSERT INTO products (name, price) VALUES
+('Product A', 19.99),
+('Product B', 29.99),
+('Product C', 39.99);
